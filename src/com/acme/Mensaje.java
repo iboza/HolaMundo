f3a004588b61b72e96449f03fc09e0122a289eda
iboza <israel.boza9@gmail.com>
2021-09-14 23:24:17 -0500

    Se integro la clase Mensaje

---
+++ src/com/acme/Mensaje.java
@@ -0,0 +1,18 @@
+package com.acme;
+
+import java.util.Scanner;
+
+public class Mensaje {
+
+    public String solicitaDatos(String mensaje) {
+
+        System.out.println(mensaje);
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+
+    }
+
+    public void saludar(String nombre) {
+        System.out.printf("Hola %s, Bienvenido a Java \n", nombre);
+    }
+}
