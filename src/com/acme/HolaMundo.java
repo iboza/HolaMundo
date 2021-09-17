af3158e16385c72846fe5a2a464b3a96e1b8cee1
iboza <israel.boza9@gmail.com>
2021-09-16 21:09:53 -0500

    Se agrego la clase hola mundo

--- src/com/acme/HolaMundo.java
+++ src/com/acme/HolaMundo.java
@@ -1,13 +1,9 @@
 package com.acme;
 
 public class HolaMundo {
+
     public static void main(String[] args) {
 
-        Mensaje msg = new Mensaje();
-        String nombre = msg.solicitaDatos("Escribe tu nombre: ");
-        String apellidos = msg.solicitaDatos("Escribe tu apellido");
-        Persona p1 = new Persona(nombre, apellidos);
-        msg.saludar(p1);
+        System.out.println("Hola Mundo!!!");
     }
-
 }
